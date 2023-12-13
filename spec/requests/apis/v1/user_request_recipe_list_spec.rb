9e8ad0051a225bdbab25d01bb8cfa8c2257711b0
Eliseu Ramos <eliseu.mec@gmail.com>
2023-12-13 14:41:49 -0300

    commit - primeira conversa

---
+++ spec/requests/apis/v1/user_request_recipe_list_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'GET api/v1/recipes/' do
+  scenario 'lista todas as receitas' do
+    # arrange
+    recipe_type = create(:recipe_type, name: 'Sobremesa')
+    create(:recipe, title: 'Manjar', cook_time: 60,
+                    recipe_type:,
+                    ingredients: 'leite condensado, leite, leite de coco',
+                    instructions: 'Misture tudo, leve ao fogo e mexa, leve a geladeira')
+    recipe_type = create(:recipe_type, name: 'Lanche')
+    create(:recipe, title: 'Hamburguer', cook_time: 10,
+                    recipe_type:,
+                    ingredients: 'hamburguer, pão de hamburguer, queijo',
+                    instructions: 'Frite o hamburguer, coloque no pão, coma')
+
+    # act
+    get '/api/v1/recipes/'
+
+    # assert
+    expect(response).to have_http_status(200)
+    expect(response.content_type).to include 'application/json'
+  end
+
+  pending 'filtra as receitas por nome ou tipo de receita'
+
+  pending 'retorna um array vazio se não houverem receitas'
+
+  pending 'avisa sobre um erro de servidor'
+end
